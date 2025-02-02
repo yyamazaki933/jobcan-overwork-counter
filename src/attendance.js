39b14e78953c2fe86d955392215d370c25d2484d
yudai.yamazaki <yudai.yamazaki@map4.jp>
2025-02-03 01:49:08 +0900

    add overwork card

--- src/attendance.js
+++ src/attendance.js
@@ -8,105 +8,100 @@ function timeToMinutes (time) {
   return minutes
 }
 
-// 1.00 のような休暇など取得日数を分に変換
-function daysToMinutes (days) {
-  const number = parseFloat(days, 10)
-  const minutes = number * 8 * 60 // 1.0 日を8時間とみなす
-  return minutes
-}
-
 function minutesToHoursMinutes (allMinutes) {
   const abs = Math.abs(allMinutes)
   const hours = Math.floor(abs / 60)
   const minutes = abs - hours * 60
   if (hours > 0) {
-    return `${hours}時間${minutes}分`
+    return `${hours}時間 ${minutes}分`
   } else {
     return `${minutes}分`
   }
 }
 
-function pastOrFuture (showedYear, showedMonth, cell, index, today) {
-  switch (cell.innerText) {
-    case '': {
-      return { past: 0, future: 0 }
-    }
-    default: {
-      const vacationTitle = cell.getAttribute('data-original-title')
-
-      // 時間休は有休ではないので稼働時間をつけない
-      if (vacationTitle.includes('時間休')) {
-        return { past: 0, future: 0 }
-      }
-
-      // 半休か全休か
-      const vacationLength = vacationTitle.includes('0.5') ? 0.5 : 1.0
-
-      // 今日自体は除きたいので、翌日の 0:00とする
-      const date = new Date(showedYear, showedMonth - 1, index + 2)
-      if (date < today) {
-        return { past: vacationLength, future: 0 }
-      } else {
-        return { past: 0, future: vacationLength }
-      }
-    }
-  }
+function makeTableRow (title, data) {
+  return `
+  <tr>
+    <th scope="row" class="jbc-text-sub">${title}</th>
+    <td>
+      <span class="info-content">${data}</span>
+    </td>
+  </tr>`
 }
 
-const messageBar = document.createElement('div')
-messageBar.style = `
-   background: rgba(30, 30, 30, 0.75);
-   display: block;
-   line-height: 1em;
-   width: 100%;
-   position: fixed;
-   bottom: 0;
-   padding: 12px;
-   z-index: 99999;
-`
-const text = document.createElement('div')
-text.style = `
-   font-size: 50px;
-   line-height: 1em;
-   color: rgb(240, 240, 240);
-`
-messageBar.append(text)
-
 try {
-  const today = new Date()
-  const [, showedYear, showedMonth] = document.querySelector('#search-result > div.row > div:nth-child(1) > div > div.card-body > table > tbody > tr:nth-child(1) > td')
-    .innerText.match(/(\d+)\D(\d+)/).map(txt => parseInt(txt, 10))
+  // ユーザー情報
+  const userInfoTBody = document.querySelector('#search-result > div.row > div:nth-child(1) > div.card > div.card-body > table > tbody')
+  const userInfo = Array
+  .from(userInfoTBody.querySelectorAll('tr'))
+  .reduce((acc, e) => {
+    acc[e.querySelector('th').innerText] = e.querySelector('td').innerText
+    return acc
+  }, {})
 
-  // 当月の有休を取得分 past と取得予定の future に仕分ける
-  const paidHolidays = Array
-    .from(document.querySelectorAll('#search-result > div.table-responsive.text-nowrap > table > tbody td:nth-child(11) > div'))
-    .map((el, index) => pastOrFuture(showedYear, showedMonth, el, index, today))
-    .reduce((last, current) => {
-      return { past: last.past + current.past, future: last.future + current.future }
-    }, { past: 0, future: 0 })
+  // 基本項目
+  const basicInfoTBody = document.querySelector('#search-result > div.row > div:nth-child(2) > div.card > div.card-body > table > tbody')
+  const basicInfo = Array
+  .from(basicInfoTBody.querySelectorAll('tr'))
+  .reduce((acc, e) => {
+    acc[e.querySelector('th').innerText] = e.querySelector('td').innerText
+    return acc
+  }, {})
 
-  // 所定過不足累計: 【昨日時点の実労働時間】-【昨日時点までに経過した所定労働日数】×【1日分の所定労働時間】
-  // @see https://jobcan.zendesk.com/hc/ja/articles/360000600182-%E6%89%80%E5%AE%9A%E5%8A%B4%E5%83%8D%E6%99%82%E9%96%93%E3%81%AE%E9%81%8E%E4%B8%8D%E8%B6%B3%E6%99%82%E9%96%93%E3%82%92%E7%A2%BA%E8%AA%8D%E3%81%99%E3%82%8B%E3%81%93%E3%81%A8%E3%81%AF%E3%81%A7%E3%81%8D%E3%81%BE%E3%81%99%E3%81%8B-
-  const laborHoursMinutes = Array
-    .from(document.querySelectorAll('#search-result > div.row > div:nth-child(3) > div.card > div.card-body > table > tbody > tr'))
+  // 労働時間
+  const statisticsTBody = document.querySelector('#search-result > div.row > div:nth-child(3) > div.card > div.card-body > table > tbody')
+  const statisticsMins = Array
+    .from(statisticsTBody.querySelectorAll('tr'))
     .reduce((acc, e) => {
       acc[e.querySelector('th').innerText] = timeToMinutes(e.querySelector('td').innerText)
       return acc
-    }, {})['所定過不足累計']
+    }, {})
+  
+  const regularMin = statisticsMins['月規定労働時間']
+  const actualMin = statisticsMins['実労働時間']
+  const overworkMin = statisticsMins['実残業時間']
+  const regularWorkday = userInfo['所定労働日数'].match(/[0-9]{2}/)[0]
+  const actualWorkday = basicInfo['実働日数']
 
-  // 過ぎた有休のみ計算。未来の有休を含めてしまうと「今日まで」の残業時間が多くなってしまうため。
-  const actualHolidayMinutes = daysToMinutes(paidHolidays.past)
+  const over45hMin = regularMin + (45 * 60)
+  const over80hMin = regularMin + (80 * 60)
+  const remain45hMin = over45hMin - actualMin
+  const remain80hMin = over80hMin - actualMin
 
-  const overAndShorts = laborHoursMinutes + actualHolidayMinutes
+  const remainWorkday = parseInt(regularWorkday, 10) - parseInt(actualWorkday, 10)
+  let ave45hMin = 0
+  let ave80hMin = 0
+  if (remainWorkday != 0) {
+    ave80hMin = remain80hMin / remainWorkday
+    ave45hMin = remain45hMin / remainWorkday
+  }
 
-  const hourMin = minutesToHoursMinutes(overAndShorts)
+  const new_jbc_card = document.createElement('div')
+  new_jbc_card.className = "card jbc-card-bordered h-100 mb-3"
+  new_jbc_card.innerHTML = `
+    <div class="card-header jbc-card-header">
+      <h5 class="card-text">残業カウンター **TEST** </h5>
+    </div>
+    <div class="card-body">
+      <table class="table jbc-table jbc-table-fixed info-contents">
+        <tbody>
+          ${makeTableRow("規定労働時間", minutesToHoursMinutes(regularMin))}
+          ${makeTableRow("実働時間", minutesToHoursMinutes(actualMin))}
+          ${makeTableRow("残業時間", minutesToHoursMinutes(overworkMin))}
+          ${makeTableRow("残業45H超過まで残り", minutesToHoursMinutes(remain45hMin))}
+          ${makeTableRow("残業80H超過まで残り", minutesToHoursMinutes(remain80hMin))}
+          ${makeTableRow("残り勤務日数", remainWorkDay + ' 日')}
+          ${makeTableRow("残業45H以内 1日平均", minutesToHoursMinutes(ave45hMin))}
+          ${makeTableRow("残業80H以内 1日平均", minutesToHoursMinutes(ave80hMin))}
+        </tbody>
+      </table>
+    </div>`
 
-  text.innerText = overAndShorts >= 0
-    ? `今日まで: ${hourMin} 残業してます`
-    : `今日まで: ${hourMin} 不足してるようです`
+  const searchResult = document.querySelector('#search-result')
+  const workRecordTable = document.querySelector('#search-result > div.table-responsive')
+  searchResult.insertBefore(new_jbc_card, workRecordTable)
+
 } catch (e) {
   console.error(e)
   text.innerText = '😱 エラーが発生しました。jobkan-helper にご報告いただけると助かります'
 }
-
-document.body.appendChild(messageBar)
