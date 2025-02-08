cc9ded4391f64e416e30c1aae6dfa9f150ae41a3
yudai.yamazaki <yudai.yamazaki@map4.jp>
2025-02-09 02:08:40 +0900

    add csv download

--- src/attendance.js
+++ src/attendance.js
@@ -11,7 +11,7 @@ function timeToMinutes (time) {
 function minutesToHoursMinutes (allMinutes) {
   let hm_str = ''
   if (allMinutes < 0) {
-    hm_str = '- '
+    hm_str = '-'
   }
   const abs = Math.abs(allMinutes)
   const hours = Math.floor(abs / 60)
@@ -21,20 +21,82 @@ function minutesToHoursMinutes (allMinutes) {
   return hm_str
 }
 
-function makeTableRow (title, data) {
-  return `
-  <tr>
-    <th scope="row" class="jbc-text-sub">${title}</th>
-    <td>
-      <span class="info-content">${data}</span>
-    </td>
-  </tr>`
+function makeTableRow (title, colums) {
+  let innerHTML = `<tr><th scope="row" class="jbc-text-sub">${title}</th>`
+  colums.forEach(colum => {
+    innerHTML += `<td><span class="info-content">${colum}</span></td>`
+  });
+  innerHTML += `</tr>`
+  return innerHTML
+}
+
+function getWorktimeTable() {
+  const table = document.querySelector('#search-result > div.table-responsive > table')
+  const data = []
+
+  const header = []
+  const theads = table.querySelectorAll('table > thead > tr > th')
+  theads.forEach(th => {
+    const item = th.innerText.replace(/(\r\n|\n|\r)/gm, "")
+    header.push(item)
+  });
+  data.push(header)
+
+  const trows = table.querySelectorAll('table > tbody > tr')
+  trows.forEach(tr => {
+    const row = []
+    let workmin = null
+    const tdatas = tr.querySelectorAll('td')
+    tdatas.forEach((td, key) => {
+      let item = td.innerText.replace(/(\r\n|\n|\r)/gm, "")
+      if (item && key === 4) {  // 勤務時間：次の残業時間計算のため取得
+        workmin = timeToMinutes(item)
+      }
+      if (workmin && key === 5) {  // 残業時間：一日ごとに-8:00して入力
+        let overmin = workmin - timeToMinutes('8:00')
+        item = minutesToHoursMinutes(overmin)
+        if (!td.innerText) {  //（空欄だったらtableに表示）
+          td.innerText = item
+        }
+      }
+      if (key === 8) {  // 打刻詳細：改行が入ってめんどくさいので無視
+        item = ''
+      }
+      row.push(item)
+    })
+    data.push(row)
+  })
+
+  // footer
+  const f0 = data.length + 1 // row idx
+  const footer0 = ["合計", "", "", "", `=SUM(E2:E${f0-1})+$B$${f0+3}-$B$${f0+3}`, `=SUM(F2:F${f0-1})+$B$${f0+3}-$B$${f0+3}`, `=SUM(G2:G${f0-1})+$B$${f0+3}-$B$${f0+3}`, `=SUM(H2:H${f0-1})+$B$${f0+3}-$B$${f0+3}`, "", ""]
+  const footer1 = ["残勤務日", `${remainWorkday}`]
+  const footer2 = ["", "所定", "残り時間", "残日平均"]
+  const footer3 = ["規定時間まで", `${minutesToHoursMinutes(regularMin)}`, `=B${f0+3}-E${f0}`, `=C${f0+3}/B${f0+1}`]
+  const footer4 = ["+45Hまで",     `${minutesToHoursMinutes(over45hMin)}`, `=B${f0+4}-E${f0}`, `=C${f0+4}/B${f0+1}`]
+  const footer5 = ["+80Hまで",     `${minutesToHoursMinutes(over80hMin)}`, `=B${f0+5}-E${f0}`, `=C${f0+5}/B${f0+1}`]
+  data.push(footer0)
+  data.push(footer1)
+  data.push(footer2)
+  data.push(footer3)
+  data.push(footer4)
+  data.push(footer5)
+
+  return data
 }
 
 function handleDownload() {
-  var content = '2,3,=a1*b1\n所定労働時間,かかかか,aaaaa\naaaaa,aaaa,aa';
+  let csvdata = ''
+  const data = getWorktimeTable()
+  data.forEach(row => {
+    let colstr = ''
+    row.forEach(col => {
+      colstr += col + ','
+    });
+    csvdata += colstr + '\n'
+  });
   var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
-  var blob = new Blob([ bom, content ], { "type" : "text/csv" });
+  var blob = new Blob([ bom, csvdata ], { "type" : "text/csv" });
   document.getElementById("download_csv").href = window.URL.createObjectURL(blob);
 }
 
@@ -43,7 +105,7 @@ new_jbc_card.className = "card jbc-card-bordered h-100 mb-3"
 
 const new_card_head = document.createElement('div')
 new_card_head.className = "card-header jbc-card-header"
-new_card_head.innerHTML = '<h5 class="card-text">**TEST** 残業カウンター</h5>'
+new_card_head.innerHTML = '<h5 class="card-text">集計情報</h5>'
 new_jbc_card.append(new_card_head)
 
 const new_card_body = document.createElement('div')
@@ -53,6 +115,11 @@ new_jbc_card.append(new_card_body)
 const searchResult = document.querySelector('#search-result')
 const workRecordTable = document.querySelector('#search-result > div.table-responsive')
 searchResult.insertBefore(new_jbc_card, workRecordTable)
+
+let regularMin = 0
+let over45hMin = 0
+let over80hMin = 0
+let remainWorkday = 0
 
 try {
   // ユーザー情報
@@ -82,9 +149,9 @@ try {
       return acc
     }, {})
   
-  const regularMin = statisticsMins['月規定労働時間']
-  const over45hMin = regularMin + (45 * 60)
-  const over80hMin = regularMin + (80 * 60)
+  regularMin = statisticsMins['月規定労働時間']
+  over45hMin = regularMin + (45 * 60)
+  over80hMin = regularMin + (80 * 60)
   const actualMin = statisticsMins['実労働時間']
   const regularWorkday = userInfo['所定労働日数'].match(/[0-9]{2}/)[0]
   const actualWorkday = basicInfo['実働日数']
@@ -106,7 +173,7 @@ try {
     remain80hMin = 0
   }
 
-  const remainWorkday = parseInt(regularWorkday, 10) - parseInt(actualWorkday, 10)
+  remainWorkday = parseInt(regularWorkday, 10) - parseInt(actualWorkday, 10)
   let aveMin = 0
   let ave45hMin = 0
   let ave80hMin = 0
@@ -116,15 +183,17 @@ try {
     ave45hMin = Math.floor(remain45hMin / remainWorkday)
   }
 
+  getWorktimeTable() // 残業時間表示のため
+
   new_card_body.innerHTML = `
     <table class="table jbc-table jbc-table-fixed info-contents">
       <tbody>
-        ${makeTableRow("規定労働時間 / +45H / +80H", `${minutesToHoursMinutes(regularMin)} / ${minutesToHoursMinutes(over45hMin)} / ${minutesToHoursMinutes(over80hMin)}`)}
-        ${makeTableRow("実働時間", minutesToHoursMinutes(actualMin))}
-        ${makeTableRow("残業時間", minutesToHoursMinutes(overworkMin))}
-        ${makeTableRow("規定時間まで残り", `${minutesToHoursMinutes(remainMin)} ( 1日平均: ${minutesToHoursMinutes(aveMin)} )`)}
-        ${makeTableRow("+45Hまで残り", `${minutesToHoursMinutes(remain45hMin)} ( 1日平均: ${minutesToHoursMinutes(ave45hMin)} )`)}
-        ${makeTableRow("+80Hまで残り", `${minutesToHoursMinutes(remain80hMin)} ( 1日平均: ${minutesToHoursMinutes(ave80hMin)} )`)}
+        ${makeTableRow("実働時間", [minutesToHoursMinutes(actualMin), '', ''])}
+        ${makeTableRow("残業時間", [minutesToHoursMinutes(overworkMin), '', ''])}
+        ${makeTableRow("残り日数", [remainWorkday, '', ''])}
+        ${makeTableRow("規定時間", [minutesToHoursMinutes(regularMin), `${minutesToHoursMinutes(over45hMin)} (+45H)`, `${minutesToHoursMinutes(over80hMin)} (+80H)`])}
+        ${makeTableRow("残り時間", [minutesToHoursMinutes(remainMin),minutesToHoursMinutes(remain45hMin), minutesToHoursMinutes(remain80hMin)])}
+        ${makeTableRow("1日平均", [minutesToHoursMinutes(aveMin), minutesToHoursMinutes(ave45hMin), minutesToHoursMinutes(ave80hMin)])}
       </tbody>
     </table>
     `
