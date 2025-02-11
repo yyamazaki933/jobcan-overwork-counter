fa4df668122a7d9657114d6b54a5369b5d71f475
yudai.yamazaki <yudai.yamazaki@map4.jp>
2025-02-11 22:18:43 +0900

    fix today's remain days and overwork

--- src/attendance.js
+++ src/attendance.js
@@ -32,7 +32,6 @@ function makeTableRow (title, colums) {
 
 function getWorktimeTable() {
   const table = document.querySelector('#search-result > div.table-responsive > table')
-  const data = []
 
   const header = []
   const theads = table.querySelectorAll('table > thead > tr > th')
@@ -40,62 +39,64 @@ function getWorktimeTable() {
     const item = th.innerText.replace(/(\r\n|\n|\r)/gm, "")
     header.push(item)
   });
-  data.push(header)
+  _worktime_data.push(header)
 
   const trows = table.querySelectorAll('table > tbody > tr')
   trows.forEach(tr => {
     const row = []
     let workmin = null
     let holiday = false
+    let wipday = false
+  
     const tdatas = tr.querySelectorAll('td')
     tdatas.forEach((td, key) => {
       let item = td.innerText.replace(/(\r\n|\n|\r)/gm, "")
-      if (item && key === 1) {  // 休日区分：休日出勤の判別のため取得
+
+      if (key === 1 && item) {  // 休日区分：休日出勤の判別のため取得
         holiday = true
       }
-      if (item && key === 4) {  // 勤務時間：次の残業時間計算のため取得
-        workmin = timeToMinutes(item)
+
+      if (key === 3 && item.match(/勤務中/)) {  // 退勤時間：勤務中判別のため取得
+        wipday = true
+        _contain_wipday = true
       }
-      if (workmin && key === 5) {  // 残業時間：一日ごとに-8:00して入力
-        let overmin = workmin - timeToMinutes('8:00')
-        if (holiday) { // 休日出勤の場合は勤務時間をすべて残業時間に加算
-          overmin = workmin
+
+      if (key === 4) {  // 勤務時間：次の残業時間計算のため取得
+        if (item) {
+          workmin = timeToMinutes(item)
+        // } else if (!holiday) {  // 空欄で、かつ平日の場合(tableに表示)
+        //   workmin = 0
+        //   td.innerText = minutesToHoursMinutes(0)
         }
-        item = minutesToHoursMinutes(overmin)
-        if (!td.innerText) {  //（空欄だったらtableに表示）
+      }
+
+      if (key === 5 && workmin !== null) {  // 残業時間
+        let day_overmin = workmin - timeToMinutes('8:00')
+        if (holiday) {  // 休日の場合は所定時間を引かない
+          day_overmin = workmin
+        }
+        item = minutesToHoursMinutes(day_overmin)
+        if (!td.innerText) {  // 空欄だったらtableに表示
           td.innerText = item
         }
+        if (!wipday) {  // 退勤済の場合は月残業に加算
+          _overworkMin += day_overmin
+        }
       }
+
       if (key === 8) {  // 打刻詳細：改行が入ってめんどくさいので無視
         item = ''
       }
+
       row.push(item)
     })
-    data.push(row)
+    _worktime_data.push(row)
   })
-
-  // footer
-  const f0 = data.length + 1 // row idx
-  const footer0 = ["合計", "", "", "", `=SUM(E2:E${f0-1})+$B$${f0+3}-$B$${f0+3}`, `=SUM(F2:F${f0-1})+$B$${f0+3}-$B$${f0+3}`, `=SUM(G2:G${f0-1})+$B$${f0+3}-$B$${f0+3}`, `=SUM(H2:H${f0-1})+$B$${f0+3}-$B$${f0+3}`, "", ""]
-  const footer1 = ["残勤務日", `${remainWorkday}`]
-  const footer2 = ["", "規定労働時間", "規定+45H", "規定+80H"]
-  const footer3 = ["所定時間", `${minutesToHoursMinutes(regularMin)}`, `${minutesToHoursMinutes(over45hMin)}`, `${minutesToHoursMinutes(over80hMin)}`]
-  const footer4 = ["残り時間", `=B${f0+3}-$E$${f0}`, `=C${f0+3}-$E$${f0}`, `=D${f0+3}-$E$${f0}`]
-  const footer5 = ["1日平均", `=B${f0+4}/$B$${f0+1}`, `=C${f0+4}/$B$${f0+1}`, `=D${f0+4}/$B$${f0+1}`]
-  data.push(footer0)
-  data.push(footer1)
-  data.push(footer2)
-  data.push(footer3)
-  data.push(footer4)
-  data.push(footer5)
-
-  return data
 }
 
 function handleDownload() {
   let csvdata = ''
-  const data = getWorktimeTable()
-  data.forEach(row => {
+  _worktime_data.forEach(row => {
     let colstr = ''
     row.forEach(col => {
       colstr += col + ','
@@ -123,10 +124,9 @@ const searchResult = document.querySelector('#search-result')
 const workRecordTable = document.querySelector('#search-result > div.table-responsive')
 searchResult.insertBefore(new_jbc_card, workRecordTable)
 
-let regularMin = 0
-let over45hMin = 0
-let over80hMin = 0
-let remainWorkday = 0
+let _overworkMin = 0
+let _contain_wipday = false
+let _worktime_data = []
 
 try {
   // ユーザー情報
@@ -156,16 +156,16 @@ try {
       return acc
     }, {})
   
-  regularMin = statisticsMins['月規定労働時間']
-  over45hMin = regularMin + (45 * 60)
-  over80hMin = regularMin + (80 * 60)
+  const regularMin = statisticsMins['月規定労働時間']
+  const over45hMin = regularMin + (45 * 60)
+  const over80hMin = regularMin + (80 * 60)
   const actualMin = statisticsMins['実労働時間']
   const regularWorkday = userInfo['所定労働日数'].match(/[0-9]{2}/)[0]
-  const actualWorkday = basicInfo['平日出勤日数']
+  const actualWorkday = basicInfo['実働日数']
   const titleYearMonth = userInfo['年月']
   const staffCode = userInfo['スタッフコード']
   
-  const overworkMin = actualMin - (actualWorkday * 8 * 60)
+  getWorktimeTable()
 
   let remainMin = regularMin - actualMin
   let remain45hMin = over45hMin - actualMin
@@ -180,7 +180,15 @@ try {
     remain80hMin = 0
   }
 
-  remainWorkday = parseInt(regularWorkday, 10) - parseInt(actualWorkday, 10)
+  let remainWorkday = parseInt(regularWorkday, 10) - parseInt(actualWorkday, 10)
+  if (remainWorkday < 0) {  // 所定勤務日数を超えている場合
+    remainWorkday = 0
+  } else {
+    if (_contain_wipday) {  // 勤務中がある場合は残り日数に当日を加える
+      remainWorkday += 1
+    }
+  }
+
   let aveMin = 0
   let ave45hMin = 0
   let ave80hMin = 0
@@ -188,21 +196,32 @@ try {
     aveMin    = Math.floor(remainMin    / remainWorkday)
     ave80hMin = Math.floor(remain80hMin / remainWorkday)
     ave45hMin = Math.floor(remain45hMin / remainWorkday)
-  } else {
-    remainWorkday = 0
   }
 
-  getWorktimeTable() // 残業時間表示のため
+  // footer
+  const f0 = _worktime_data.length + 1 // row idx
+  const footer0 = ["今日までの合計", "", "", "", `=SUM(E2:E${f0-1})+$B$${f0+3}-$B$${f0+3}`, `=SUM(F2:F${f0-1})+$B$${f0+3}-$B$${f0+3}`, "", "", "", ""]
+  const footer1 = ["残勤務日", `${remainWorkday}`]
+  const footer2 = ["", "規定労働時間", "規定+45H", "規定+80H"]
+  const footer3 = ["所定時間", `${minutesToHoursMinutes(regularMin)}`, `${minutesToHoursMinutes(over45hMin)}`, `${minutesToHoursMinutes(over80hMin)}`]
+  const footer4 = ["残り時間", `=B${f0+3}-$E$${f0}`, `=C${f0+3}-$E$${f0}`, `=D${f0+3}-$E$${f0}`]
+  const footer5 = ["1日平均", `=B${f0+4}/$B$${f0+1}`, `=C${f0+4}/$B$${f0+1}`, `=D${f0+4}/$B$${f0+1}`]
+  _worktime_data.push(footer0)
+  _worktime_data.push(footer1)
+  _worktime_data.push(footer2)
+  _worktime_data.push(footer3)
+  _worktime_data.push(footer4)
+  _worktime_data.push(footer5)
 
   new_card_body.innerHTML = `
     <table class="table jbc-table jbc-table-fixed info-contents">
       <tbody>
         ${makeTableRow("実働時間", [minutesToHoursMinutes(actualMin), '', ''])}
-        ${makeTableRow("残業時間", [minutesToHoursMinutes(overworkMin), '', ''])}
-        ${makeTableRow("残り日数　※今日を除く", [remainWorkday, '', ''])}
+        ${makeTableRow("残業時間　※勤務中を除く", [minutesToHoursMinutes(_overworkMin), '', ''])}
+        ${makeTableRow("残り日数", [remainWorkday, '', ''])}
         ${makeTableRow("規定時間", [minutesToHoursMinutes(regularMin), `${minutesToHoursMinutes(over45hMin)} (+45H)`, `${minutesToHoursMinutes(over80hMin)} (+80H)`])}
         ${makeTableRow("残り時間", [minutesToHoursMinutes(remainMin),minutesToHoursMinutes(remain45hMin), minutesToHoursMinutes(remain80hMin)])}
-        ${makeTableRow("1日平均　※今日を除く", [minutesToHoursMinutes(aveMin), minutesToHoursMinutes(ave45hMin), minutesToHoursMinutes(ave80hMin)])}
+        ${makeTableRow("1日平均", [minutesToHoursMinutes(aveMin), minutesToHoursMinutes(ave45hMin), minutesToHoursMinutes(ave80hMin)])}
       </tbody>
     </table>
   `
